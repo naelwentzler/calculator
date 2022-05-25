98acab45d53bc3e0f27a89f5f22fd2fd0310679b
Nael WENTZLER <wtznael@gmail.com>
2022-05-25 12:05:59 +0200

    Ajout de somme, du controler et du test

---
+++ src/test/java/com/naelwentzler/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,22 @@
+package com.naelwentzler.calculator;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	private Calculator calculator = new Calculator();
+
+    @Test
+    public void testSum(){
+        assertEquals(5,calculator.sum(2, 3));
+    }
+}
